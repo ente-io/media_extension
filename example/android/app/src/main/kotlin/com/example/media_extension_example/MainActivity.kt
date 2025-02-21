abdf93071ee03e8aa03773f6766d052380cd26a3
Prateek Sunal <prtksunal@gmail.com>
2025-02-21 19:38:44 +0530

    fix: bump everything

---
+++ example/android/app/src/main/kotlin/com/example/media_extension_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.media_extension_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/io/ente/photos/media_extension_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.ente.photos.media_extension_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
